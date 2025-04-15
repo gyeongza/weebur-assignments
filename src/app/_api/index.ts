86400c5f7897622b2c1a34a14d606ad4e9c08e44
gyeongza <gyeongza@naver.com>
2025-04-15 13:56:59 +0900

    chore: 라우팅 그룹 적용 (home)

--- src/app/_api/index.ts
+++
@@ -1,9 +0,0 @@
-import api from '../_lib/axios';
-import { ProductsResponse } from '../_type';
-
-export const HomeApi = {
-  getProducts: async () => {
-    const response = await api.get<ProductsResponse>('/products');
-    return response.data;
-  },
-};
